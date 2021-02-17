34620245677a03cca19b2051e0b59d92cc7a79bf
Alberto Fernandez Hernandez <45654081+AlbertoUAH@users.noreply.github.com>
2021-02-17 09:09:14 +0100

    bagging: tuneando nodesize

--- bagging.R
+++ bagging.R
@@ -44,21 +44,20 @@ legend("topright", legend = c("5 variables","7 variables") ,
 
 # Tunning modelos
 n.tree <- 2000
-nodesizes.1 <- list(5, 10, 20, 30, 40, 50)
+nodesizes.1 <- list(5, 10, 20, 30, 40, 50, 100, 150, 200)
 # Sampsize maximo: (k-1) * n => 7043 / 5 = 1408 y se utilizan (4/5) * 7043 = 5634 obs.
 # Conclusion: sampsize maximo: 5633 obs. (de forma aproximada)
-sampsizes <- list(TRUE, 500, 1000, 2000, 3000, 4000, 5000)
+sampsizes <- list(1, 500, 1000, 2000, 3000, 4000, 5000)
 
 lista.rf.1 <- list()
-apply(as.data.frame(expand.grid(nodesizes.1, sampsizes)), 1, function(tunning) {
-  param1 <- tunning$Var1; param2 <- tunning$Var2
+sapply(nodesizes.1, function(param1) {
   salida <- cruzadarfbin(data=telco.data.final, vardep=vardep,
                  listconti=variables.candidato.1,
                  listclass=c(""),
                  grupos=5,sinicio=1234,repe=5,nodesize=param1,
-                 mtry=mtry.1,ntree=n.tree,replace=param2)
-  cat("NODESIZE-", tunning$Var1, "-SAMPSIZE:", tunning$Var2, "-> FINISHED\n")
-  salida$modelo <- paste0("NODESIZE-", tunning$Var1, "-SAMPSIZE:", tunning$Var2)
+                 mtry=mtry.1,ntree=n.tree)
+  cat("NODESIZE-", param1, "-> FINISHED\n")
+  salida$modelo <- paste0("NODESIZE-", param1)
   lista.rf.1 <- c(lista.rf.1, list(salida))
 })
 
