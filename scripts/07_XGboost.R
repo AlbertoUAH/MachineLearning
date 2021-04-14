538ce801f9788eee7b1e216107fd663c388e4940
AlbertoUAH <45654081+AlbertoUAH@users.noreply.github.com>
2021-04-14 23:56:57 +0200

    ...

--- scripts/07_XGboost.R
+++ scripts/07_XGboost.R
@@ -36,14 +36,29 @@ var_modelo2 <- c("mortality_rsi", "bmi", "month.8", "Age")
 #-- Probamos con un tuneo inicial
 #   Modelo 1
 set.seed(1234)
-xgbmgrid <- expand.grid(eta=c(0.1,0.05,0.03,0.01,0.001), nrounds=c(100,500,1000,5000),
+xgbmgrid <- expand.grid(min_child_weight=c(5,10,20),eta=c(0.1,0.05,0.03,0.01,0.001), nrounds=c(100,500,1000,5000),
                         max_depth=6,gamma=0,colsample_bytree=1,subsample=1)
 
-control<-trainControl(method = "cv",number=5,repeats = 5,
+control<-trainControl(method = "repeatedcv",number=5,repeats = 5,
                       savePredictions = "all",classProbs=TRUE)
-xgbm<- train(paste0(target,"~",paste0(var_modelo1, collapse = "+")),data=surgical_dataset,
+xgbm<- train(as.formula(paste0(target,"~",paste0(var_modelo1, collapse = "+"))),data=surgical_dataset,
              method="xgbTree",trControl=control,tuneGrid=xgbmgrid,verbose=FALSE)
 xgbm
-plot(xgbm)
+plot(xgbm, main = "Tuneo parametros XGBoost Modelo 1")
 
 # Modelo 2
+set.seed(1234)
+xgbm_2 <- train(as.formula(paste0(target,"~",paste0(var_modelo2, collapse = "+"))),data=surgical_dataset,
+                method="xgbTree",trControl=control,tuneGrid=xgbmgrid,verbose=FALSE)
+xgbm_2
+plot(xgbm_2, main = "Tuneo parametros XGBoost Modelo 2")
+
+
+#-- En ambos modelos, practicamente llegamos a la misma conclusion: debe tomarse un shrinkage bajo y un numero bajo de iteraciones
+#   (entre 100 y 500), o bien otro extremo y aplicar un elevado numero de irteraciones (en torno a 5000) y un shrinkage bajo, 
+#   aunque la diferencia entre ambos es prácticamente nula. Ademas, y dado que las diferencias entre los tres modelos son pequeñas,
+#   es recomendable emplear un valor min_child_weight alto (20).
+#   Por tanto, probamos a fijar parametros como min_child_weight y eta
+
+
+
